4810d8fd79fb52fb8ce7e7ab424b6037f15aa897
fengbear <sir_fengfan@163.com>
2018-10-30 21:28:14 +0800

    random policy done

--- lovebird_markov.py
+++ lovebird_markov.py
@@ -12,16 +12,27 @@ class GameConfig:
     caption = 'lovebird-ff'
     window_size = (960, 540)
     grid_size = (60, 60)
+    row = window_size[1] // grid_size[1]
+    col = window_size[0] // grid_size[0]
+
     brick_size = (138, 793)
+    bricks_pos = [5, 5, 10, 10]
+    bricks_height = [4, 3, 3, 4]
+
     bird_size = (780, 690)
+    female_bird_pos = col-1
+
     fps = 30
     save_number = 0
-
     loop_flag = False
 
     root_path = '.'
     resource_path = 'assets'
     save_path = 'output'
+
+    reward_category = [-1, -5, 1]
+    actions = ['e', 'w', 's', 'n']
+    gamma = 1
 
 
 class GameItem(pygame.sprite.Sprite):
@@ -89,13 +100,13 @@ class Bird(GameItem):
             self.update_mask()
 
     def update(self, step, direction):
-        if direction == 0:  # Down
+        if direction == 's':  # Down
             self.rect.move_ip(0, step)
-        if direction == 1:  # Right
+        if direction == 'e':  # Right
             self.rect.move_ip(step, 0)
-        if direction == 2:  # Up
+        if direction == 'n':  # Up
             self.rect.move_ip(0, -step)
-        if direction == 3:  # Left
+        if direction == 'w':  # Left
             self.rect.move_ip(-step, 0)
 
     def collision(self, step, bricks, window_size):
@@ -131,7 +142,7 @@ class LoveBirdGame():
         self.clock = pygame.time.Clock()
         self.history_grid = Grid((255, 0, 0), 5, 'rect')
 
-    def loop(self, bg, grid, bricks, birds, mode):
+    def loop(self, bg, grid, bricks, birds, action_advice):
         while True:
             self.clock.tick(GameConfig.fps)
 
@@ -154,12 +165,18 @@ class LoveBirdGame():
             self.blit(bg, grid, bricks, birds, self.history_grid)
 
             if GameConfig.loop_flag:
-                action = male_bird.collision(GameConfig.grid_size[0], bricks, GameConfig.window_size)
+                # action = male_bird.collision(GameConfig.grid_size[0], bricks, GameConfig.window_size)
+
+                state_x = male_bird.rect[1] // GameConfig.grid_size[1]
+                state_y = male_bird.rect[0] // GameConfig.grid_size[0]
+
+                action = action_advice([state_x, state_y])
+                print([state_x, state_y], action)
                 male_bird.update(GameConfig.grid_size[0], action)
                 if male_bird.find_mate(female_bird):
                     GameConfig.loop_flag = not GameConfig.loop_flag
-                    self.reset()
-                    male_bird.reset()
+                    # self.reset()
+                    # male_bird.reset()
 
             pygame.display.update()
 
@@ -173,6 +190,84 @@ class LoveBirdGame():
 
     def reset(self):
         self.history_grid.containers = []
+
+
+class Markov():
+    def __init__(self, reward_grid, reward_category, actions, gamma):
+        self.i_rewards = reward_grid  # immediate_reward
+
+        self.s_states = []  # search
+        self.c_states = []  # crash
+        self.e_states = []  # end
+        for i, row in enumerate(reward_grid):
+            for j, reward in enumerate(row):
+                if reward == reward_category[0]:
+                    self.s_states.append([i, j])
+                if reward == reward_category[1]:
+                    self.c_states.append([i, j])
+                if reward == reward_category[2]:
+                    self.e_states.append([i, j])
+
+        self.actions = actions
+        self.gamma = gamma
+
+        self.v_values = np.zeros_like(reward_grid, dtype=np.float)
+        for state in self.c_states:
+            self.v_values[state[0]][state[1]] = -100000
+
+    # pi(a|s)
+    def pi_function(self, state, action):
+        return 1/len(self.actions)
+
+    # v_pi(s)
+    def state_value_function(self, state):
+        value = 0
+        for action in self.actions:
+            value += self.pi_function(state, action) * self.action_value_function(state, action)
+        return value
+
+    # q_pi(s,a)
+    def action_value_function(self, state, action):
+        next_state = self.get_next_state(state, action)
+        if next_state == state:
+            return self.i_rewards[state[0]][state[1]] + self.gamma * self.v_values[state[0]][state[1]]
+        else:
+            return self.i_rewards[state[0]][state[1]] + self.gamma * self.v_values[next_state[0]][next_state[1]]
+
+    def policy_evaluation(self):
+        k = 0
+        while True:
+            last_v_values = self.v_values.copy()
+            for i, state in enumerate(self.s_states):
+                self.v_values[state[0]][state[1]] = self.state_value_function(state)
+            k += 1
+            last_v_values = np.around(last_v_values, decimals=0)
+            self.v_values = np.around(self.v_values, decimals=0)
+            if (last_v_values == self.v_values).all():
+                break
+        print(k)
+
+    def get_next_state(self, state, action):
+        next_state = state.copy()
+        if action == 'e':
+            next_state[1] += 1
+        if action == 'w':
+            next_state[1] -= 1
+        if action == 's':
+            next_state[0] += 1
+        if action == 'n':
+            next_state[0] -= 1
+        if not (next_state in self.s_states or next_state in self.e_states):
+            next_state = state.copy()
+        return next_state
+
+    def give_action_advice(self, state):
+        v_values_candidate = []
+        for action in self.actions:
+            next_state = self.get_next_state(state, action)
+            v_values_candidate.append(self.v_values[next_state[0]][next_state[1]])
+        print(v_values_candidate)
+        return self.actions[v_values_candidate.index(max(v_values_candidate))]
 
 
 def game_env_init():
@@ -189,7 +284,7 @@ def game_env_init():
     grid.containers += v_lines
 
     bricks = pygame.sprite.Group()
-    for i, location_item, height_item in zip(range(4), [5, 5, 10, 10], [4, 3, 3, 4]):
+    for i, location_item, height_item in zip(range(4), GameConfig.bricks_pos, GameConfig.bricks_height):
         brick = Brick(os.path.join(GameConfig.root_path, GameConfig.resource_path, 'brick.png'), GameConfig.brick_size, GameConfig.grid_size)
         height = brick.image.get_size()[1] - height_item * GameConfig.grid_size[1]
         flip_flag = True
@@ -209,6 +304,39 @@ def game_env_init():
     return game, bg, grid, bricks, birds
 
 
+def markov_env_init():
+    reward_category = GameConfig.reward_category
+    i_rewards = np.ones((GameConfig.row, GameConfig.col), dtype=np.int) * reward_category[0]
+    for i, pos, height in zip(range(len(GameConfig.bricks_pos)), GameConfig.bricks_pos, GameConfig.bricks_height):
+        for j in range(height):
+            if i % 2 == 0:
+                i_rewards[GameConfig.row-j-1][pos] = reward_category[1]
+            else:
+                i_rewards[j][pos] = reward_category[1]
+    i_rewards[0][GameConfig.female_bird_pos] = reward_category[2]
+    return i_rewards, reward_category
+
+
+def test_policy_evaluation():
+    reward_grid = -np.ones((4, 4), dtype=np.int)
+    reward_grid[0][0] = 0
+    reward_grid[3][3] = 0
+    markov = Markov(reward_grid, [-1, 1, 0], GameConfig.actions, GameConfig.gamma)
+    print(markov.i_rewards)
+    print(markov.v_values)
+    markov.policy_evaluation()
+    print(markov.v_values)
+
+
 if __name__ == '__main__':
+    np.set_printoptions(linewidth=400)
+    markov = Markov(*markov_env_init(), GameConfig.actions, GameConfig.gamma)
+    # print(markov.i_rewards)
+    # print(markov.v_values)
+    markov.policy_evaluation()
+    print(markov.v_values)
+    # print(markov.give_action_advice([0, 0]))
     game, *game_obj = game_env_init()
-    game.loop(*game_obj, mode='value')
+    game.loop(*game_obj, action_advice=markov.give_action_advice)
+
+    # test_policy_evaluation()
