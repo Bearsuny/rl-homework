180b47f7eebff48d57b2f2b90a78fc2acc01800a
fengbear <sir_fengfan@163.com>
2018-11-06 17:00:28 +0800

    monte_carlo done

--- lovebird.py
+++ lovebird.py
@@ -37,8 +37,10 @@ class GameConfig:
     markov_mode_space = ['random', 'policy_iteration', 'value_iteration']
     markov_mode_count = 0
 
-    monte_mode_space = ['exploring_starts']
+    monte_mode_space = ['exploring_starts', 'on_policy']
     monte_mode_count = 0
+
+    monte_on_policy_epsilon = 0.1
 
 
 class GameItem(pygame.sprite.Sprite):
@@ -181,14 +183,20 @@ class LoveBirdGame():
                         GameConfig.loop_flag = not GameConfig.loop_flag
                         self.reset()
                         male_bird.reset()
-                        policy.mode = policy.mode_space[GameConfig.markov_mode_count]
-                        if GameConfig.markov_mode_count < len(GameConfig.markov_mode_space) - 1:
-                            GameConfig.markov_mode_count += 1
+                        # policy.mode = policy.mode_space[GameConfig.markov_mode_count]
+                        # if GameConfig.markov_mode_count < len(GameConfig.markov_mode_space) - 1:
+                        #     GameConfig.markov_mode_count += 1
+                        # else:
+                        #     GameConfig.markov_mode_count = 0
+                        # policy.mode = policy.mode_space[1]
+                        # policy.change_mode(policy.mode)
+                        # self.v_values_grid.containers = policy.v_values
+                        policy.mode = policy.mode_space[GameConfig.monte_mode_count]
+                        if GameConfig.monte_mode_count < len(GameConfig.monte_mode_space) - 1:
+                            GameConfig.monte_mode_count += 1
                         else:
-                            GameConfig.markov_mode_count = 0
-                        policy.mode = policy.mode_space[2]
+                            GameConfig.monte_mode_count = 0
                         policy.change_mode(policy.mode)
-                        self.v_values_grid.containers = policy.v_values
                     if event.key == K_ESCAPE:
                         pygame.image.save(self.screen, os.path.join(GameConfig.root_path, GameConfig.save_path, f'{GameConfig.save_number}.png'))
                         GameConfig.save_number += 1
@@ -356,7 +364,7 @@ class Markov():
 
 
 class MonteCarlo():
-    def __init__(self, reward_grid, reward_category, actions, mode_space):
+    def __init__(self, reward_grid, reward_category, actions, gamma, mode_space, *args):
         self.i_rewards = reward_grid  # immediate_reward
 
         self.s_states = []  # search
@@ -373,21 +381,145 @@ class MonteCarlo():
 
         self.actions = actions
 
-        self.pi_values = np.zeros((reward_grid.shape[0], reward_grid.shape[1]), dtype=np.int)
-        self.q_values = np.zeros((reward_grid.shape[0], reward_grid.shape[1], len(actions)), dtype=np.float)
-        self.return_values = np.zeros_like(self.q_values, dtype=np.float)
+        self.q_values = np.ones((reward_grid.shape[0], reward_grid.shape[1], len(actions)), dtype=np.float) * -10000
+        self.return_values = np.zeros((reward_grid.shape[0], reward_grid.shape[1], len(actions)), dtype=np.int)
+        self.return_values_count = np.zeros((reward_grid.shape[0], reward_grid.shape[1], len(actions)), dtype=np.int)
+
+        self.pi_values = np.ones_like(self.return_values, dtype=np.float) * (1 / len(self.actions))
+
+        self.gamma = gamma
 
         self.mode_space = mode_space
         self.mode = None
 
-    def exploring_starts(self):
-        # generate _episode
-        episodes = []
-        for i in range(2000):
+        self.epsilon = args[0]
+
+    def change_mode(self, mode):
+        self.q_values = np.ones_like(self.q_values, dtype=np.float) * -10000
+        self.return_values = np.zeros_like(self.return_values, dtype=np.int)
+        self.return_values_count = np.zeros_like(self.return_values_count, dtype=np.int)
+        self.pi_values = np.ones_like(self.return_values, dtype=np.float) * (1 / len(self.actions))
+
+        if self.mode == 'exploring_starts':
+            # self.exploring_starts()
+            self.q_values = np.load('./output/q_values.npy').reshape(self.q_values.shape)
+        if self.mode == 'on_policy':
+            # self.on_policy()
+            self.pi_values = np.load('./output/pi_values.npy').reshape(self.pi_values.shape)
+
+
+    # def generate_episode(self, state, episode):
+    #     if state in self.e_states:
+    #         return
+    #     if self.mode == 'exploring_starts':
+    #         action = np.random.choice(self.actions)
+    #     elif self.mode == 'on_policy':
+    #         action = np.random.choice(self.actions, p=self.pi_values[state[0]][state[1]])
+    #     next_state = state.copy()
+    #     if action == 'e':
+    #         next_state[1] += 1
+    #     if action == 'w':
+    #         next_state[1] -= 1
+    #     if action == 's':
+    #         next_state[0] += 1
+    #     if action == 'n':
+    #         next_state[0] -= 1
+    #     if not (next_state in self.s_states or next_state in self.e_states):
+    #         next_state = state.copy()
+
+    #     episode.append((state, action))
+    #     return self.generate_episode(next_state, episode)
+
+    def generate_episode_recurrent(self, state):
+        episode = []
+        state_init = state.copy()
+        while episode == [] or len(episode) > 1000:
             episode = []
-            state = np.random.choice(self.s_states)
-            action = np.random.choice(self.actions)
-            print(state, action)
+            state = state_init.copy()
+            next_state = state_init.copy()
+            while not state in self.e_states:
+                if self.mode == 'exploring_starts':
+                    action = np.random.choice(self.actions)
+                elif self.mode == 'on_policy':
+                    action = np.random.choice(self.actions, p=self.pi_values[state[0]][state[1]])
+                if action == 'e':
+                    next_state[1] += 1
+                if action == 'w':
+                    next_state[1] -= 1
+                if action == 's':
+                    next_state[0] += 1
+                if action == 'n':
+                    next_state[0] -= 1
+                if not (next_state in self.s_states or next_state in self.e_states):
+                    next_state = state.copy()
+                episode.append((state, action))
+                state = next_state.copy()
+                if len(episode) > 1000:
+                    break
+        return episode
+
+    def generate_episodes(self, num):
+        episodes = []
+        for i in range(num):
+            for j, state in enumerate(self.s_states):
+                episode = self.generate_episode_recurrent(state)
+                print(f'{i} epoch, {j} state, {len(episode)}')
+                episodes.append(episode)
+        return episodes
+
+    def calculate_g_first_visit(self, episodes):
+        for k, episode in enumerate(episodes):
+            # print(f'{k} Processing...')
+            g = 0
+            for i in range(len(episode)-1, -1, -1):
+                value = episode[i]
+                g = self.i_rewards[value[0][0]][value[0][1]] + g * self.gamma
+                if episode.index(value) == i:
+                    self.return_values[value[0][0]][value[0][1]][self.actions.index(value[1])] += g
+                    self.return_values_count[value[0][0]][value[0][1]][self.actions.index(value[1])] += 1
+
+    def exploring_starts(self):
+        episodes = self.generate_episodes(10000)
+        self.calculate_g_first_visit(episodes)
+
+        for i in range(self.q_values.shape[0]):
+            for j in range(self.q_values.shape[1]):
+                for k in range(self.q_values.shape[2]):
+                    if self.return_values_count[i][j][k] != 0:
+                        self.q_values[i][j][k] = self.return_values[i][j][k] / self.return_values_count[i][j][k]
+
+        print(self.q_values)
+
+    def on_policy(self):
+        for c in range(1000):
+            print(f'{c} ....')
+            state = [0, 0]
+            episode = self.generate_episode_recurrent(state)
+            episodes = []
+            episodes.append(episode)
+            self.calculate_g_first_visit(episodes)
+
+            for i in range(self.q_values.shape[0]):
+                for j in range(self.q_values.shape[1]):
+                    for k in range(self.q_values.shape[2]):
+                        if self.return_values_count[i][j][k] != 0:
+                            self.q_values[i][j][k] = self.return_values[i][j][k] / self.return_values_count[i][j][k]
+
+            for i in range(self.pi_values.shape[0]):
+                for j in range(self.pi_values.shape[1]):
+                    for k, action in enumerate(self.actions):
+                        if action == self.actions[np.argmax(self.q_values[i][j])]:
+                            self.pi_values[i][j][k] = 1 - self.epsilon + self.epsilon / len(self.actions)
+                        else:
+                            self.pi_values[i][j][k] = self.epsilon / len(self.actions)
+
+    def give_action_advice(self, state):
+        if self.mode == 'exploring_starts':
+            print(self.q_values[state[0]][state[1]], self.actions[np.argmax(self.q_values[state[0]][state[1]])])
+            return self.actions[np.argmax(self.q_values[state[0]][state[1]])]
+        elif self.mode == 'on_policy':
+            print(self.pi_values[state[0]][state[1]], self.actions[np.argmax(self.pi_values[state[0]][state[1]])])
+            return self.actions[np.argmax(self.pi_values[state[0]][state[1]])]
 
 
 def game_env_init():
@@ -454,12 +586,14 @@ def test_policy_evaluation():
 
 if __name__ == '__main__':
     np.set_printoptions(linewidth=400)
+    sys.setrecursionlimit(10**6)
 
     # markov = Markov(*markov_env_init(), GameConfig.actions, GameConfig.gamma, GameConfig.markov_mode_space)
     # game, *game_obj = game_env_init()
     # game.loop(*game_obj, policy=markov)
 
-    monte_carlo = MonteCarlo(*monte_carlo_env_init(), GameConfig.actions, GameConfig.monte_mode_space)
-    monte_carlo.exploring_starts()
+    monte_carlo = MonteCarlo(*monte_carlo_env_init(), GameConfig.actions, GameConfig.gamma, GameConfig.monte_mode_space, GameConfig.monte_on_policy_epsilon)
+    game, *game_obj = game_env_init()
+    game.loop(*game_obj, monte_carlo)
 
     # test_policy_evaluation()
